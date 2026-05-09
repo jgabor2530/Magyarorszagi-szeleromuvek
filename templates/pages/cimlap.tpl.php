59a7ab2af9e9a79eb2e1aaa7e8321d13ff1a2a30
Juhász Gábor <jgabor2530@yahoo.com>
2026-05-09 20:17:47 +0200

    címlap hozzáadás

---
+++ templates/pages/cimlap.tpl.php
@@ -0,0 +1,20 @@
+<h2>Magyarországi Szélerőművek</h2>
+<p>A szélerőművek a szél mozgási energiáját alakítják át elektromos árammá. Magyarországon több tucat ilyen létesítmény működik, elsősorban az északnyugati régiókban, ahol a széljárás a legkedvezőbb az energiatermelés szempontjából.</p>
+
+<div class="media-container">
+    <div class="media-box">
+        <h3>Helyi videó</h3>
+        <video width="100%" controls>
+            <source src="./videos/szel.mp4" type="video/mp4">
+            A böngészője nem támogatja a HTML5 videók lejátszását.
+        </video>
+    </div>
+    
+    <div class="media-box">
+        <h3>YouTube videó</h3>
+        <iframe width="100%" height="250" src="https://www.youtube.com/embed/Lri_mQlp_Ks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</div>
+
+<h3>Irodánk elhelyezkedése</h3>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d86283.4727142436!2d16.1554907297126!3d47.83852077539063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476dcbecc41e0ff3%3A0x400c4290c1e1c30!2sSopron!5e0!3m2!1shu!2shu!4v1684400234123!5m2!1shu!2shu" width="100%" height="300" style="border:0; margin-top: 15px; border-radius: 8px;" allowfullscreen="" loading="lazy"></iframe>
