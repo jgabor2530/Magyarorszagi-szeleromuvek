8b9a57d37c8f239aab007aeeb55b8ce8cc9dc981
Veszelszki Tamás <hhhgster@gmail.com>
2026-05-09 20:23:39 +0200

    Config file hozzáadása

---
+++ includes/config.inc.php
@@ -0,0 +1,31 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Magyarországi szélerőművek',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo.png',
+    'kepalt' => 'Szélerőmű logó',
+    'cim' => 'Magyarországi szélerőművek',
+    'motto' => 'Tiszta energia a jövőért'
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Készítette: Veszelszki Tamás (PO4YGX) / Juhász Gábor (R1HXNV)'
+);
+
+$oldalak = array(
+    '/' => array('fajl' => 'cimlap', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+    'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
+    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)), // Csak belépve látszik
+    'crud' => array('fajl' => 'crud', 'szoveg' => 'CRUD', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)), // Csak kilépve látszik
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)), // Csak belépve látszik
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
