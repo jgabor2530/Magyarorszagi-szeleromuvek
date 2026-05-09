ca09d0f58b25fc73c3be45d44860fe6e31e9d93d
Veszelszki Tamás <hhhgster@gmail.com>
2026-05-09 19:36:53 +0200

    Logicals mappa létrehozása és belép.php hozzáadása

---
+++ logicals/belep.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_POST['felhasznalo']) && isset($_POST['jelszo'])) {
+    require_once 'config.php';
+    try {
+        $dbh = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password,
+                        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
+        
+        $sqlSelect = "select id, csaladi_nev, uto_nev from felhasznalok where bejelentkezes = :bejelentkezes and jelszo = sha1(:jelszo)";
+        $sth = $dbh->prepare($sqlSelect);
+        $sth->execute(array(':bejelentkezes' => $_POST['felhasznalo'], ':jelszo' => $_POST['jelszo']));
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+        
+        if($row) {
+            $_SESSION['csn'] = $row['csaladi_nev']; 
+            $_SESSION['un'] = $row['uto_nev']; 
+            $_SESSION['login'] = $_POST['felhasznalo'];
+            header("Location: .");
+            exit;
+        } else {
+            
+            header("Location: ?belepes");
+            exit;
+        }
+    }
+    catch (PDOException $e) {
+        $errormessage = "Hiba: ".$e->getMessage();
+    }      
+}
+else {
+    header("Location: .");
+}
+?>
